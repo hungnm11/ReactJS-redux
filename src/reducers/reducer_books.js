fe89af6b9eae674d4783c8e9a2b94c38ce92c97c
Hung Nguyen <hung.nguyen@sentifi.com>
2017-07-26 13:20:56 +0700

    reactjs redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,24 @@
+export default function() {
+    return [
+        {
+            title: 'Javascript: The good part',
+            pages: 101
+        },
+        {
+            title: 'Haryy Porter',
+            pages: 89
+        },
+        {
+            title: 'The dark tower',
+            pages: 32
+        },
+        {
+            title: 'React JS: The good library',
+            pages: 100
+        },
+        {
+            title: 'React Native: The good part for app',
+            pages: 1010
+        }
+    ]
+}
